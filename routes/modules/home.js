686ac50fae10ea72c786eb643e2429dfd0cab373
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-07 15:45:38 +0800

    feat: add home page and show data from model

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
 const router = express.Router()
 
 // index 首頁
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+  .then((todos) => {return res.render('index', { todos: todos }) })
+  .catch((error) => {return res.status(422).json(error)})
 })
 
 module.exports = router
