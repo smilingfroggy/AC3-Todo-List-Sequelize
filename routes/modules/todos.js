1644d7830db95256e721223593edde11b6315945
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-07 23:53:50 +0800

    feat: add edit page, route, and feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -12,4 +12,30 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// edit page
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => {
+      todo = todo.toJSON()
+      res.render('edit', { todo })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findByPk(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === "on"
+      return todo.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// delete page
+
 module.exports = router
