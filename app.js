eb5a44f7fa9339e8d0ad5097f826e681b7ee7d37
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-07 17:11:29 +0800

    feat: add login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 const bcrypt = require('bcryptjs')
 
@@ -15,12 +16,13 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// session
+// session and passport
 app.use(session({
   secret: 'MySecret',
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
 
 // Routers
 app.use(routes)
