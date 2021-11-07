e9d1baa59d7057d4c5d1259577178e25ac7addcd
smilingfroggy <smilingfroggy@hotmail.com>
2021-11-07 16:49:07 +0800

    add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 const bcrypt = require('bcryptjs')
 
@@ -13,6 +14,13 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// session
+app.use(session({
+  secret: 'MySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // Routers
 app.use(routes)
